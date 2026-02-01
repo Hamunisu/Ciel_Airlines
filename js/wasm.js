c15a5d8f02732836993f2cbae030e6729b0b9a2a
hamunisu <199821950+Hamunisu@users.noreply.github.com>
2026-02-01 14:20:10 +0900

    v2.0.3 | color & copylight fix and more

--- js/wasm.js
+++ js/wasm.js
@@ -2,17 +2,13 @@
 import init, { search } from "../pkg/ciel.js";
 
 
-
 async function main() {
     await init();
-    console.log("wasm初期化完了");
     const dep = document.getElementById("dep");
     const arr = document.getElementById("arr");
     const enter = document.getElementById("enter");
     const result = document.getElementById("result");
-
-
-            
+       
     const Wasm = () => {
         // @ts-ignore
         const resultText = search(dep.value, arr.value);
